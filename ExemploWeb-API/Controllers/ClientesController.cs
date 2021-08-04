71e71a74075afc5e5f7a6773fcb1f5816de2d466
rafael fernando <Rafael_FOSS@hotmail.com>
2021-08-04 15:45:17 -0300

    API - Metodos Post, Get e Delete de Clientes.

---
+++ ExemploWeb-API/Controllers/ClientesController.cs
@@ -0,0 +1,34 @@
+﻿using ExemploWeb_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWeb_API.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        //listar clientes...
+        public List<Cliente> Get()
+        {
+            return clientes;
+        }
+
+        //adicionar um novo cliente...
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+                clientes.Add(new Cliente(nome));
+        }
+
+        //remover cliente...
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+    }
+}
